af9bb288fb2feba7366af05e8cf4f18b4dce078d
danielchanht <daniel.chan@htgroup.com.my>
2026-01-09 22:31:48 +0800

    add responsive design for navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,5 @@
 // components/Navbar.js
-"use client";
+'use client';
 
 import { AppBar, Container, Toolbar, Typography } from '@mui/material';
 import Link from 'next/link';
@@ -19,19 +19,20 @@ export default function Navbar({ basePath = '' }: NavbarProps) {
   ];
 
   return (
-    <nav className="sticky flex items-center justify-center shadow-sm bg-white px-4 py-2 w-auto h-18 z-50 hidden">
+    <nav className="sticky flex items-center justify-center shadow-sm bg-white px-4 py-2 w-auto min-h-[4.5rem] h-auto z-50">
       {links.map((link) => {
         const fullPath = basePath + link.href;
 
         return (
           <Link
-          key={fullPath}
-          href={fullPath}
-          className='navbar-menu'
-        >
-          {link.name}
-        </Link>
-        )
+            key={fullPath}
+            href={fullPath}
+            className="navbar-menu"
+            suppressHydrationWarning
+          >
+            {link.name}
+          </Link>
+        );
       })}
     </nav>
   );
